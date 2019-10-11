d101540e0b387788fd6f5fb756af0e666a7ca535
Randy Lashley <rlashley415@gmail.com>
2019-10-10 23:08:43 -0500

    Added methods, new url, changed gradle build file, refactored

--- src/main/java/tech/lashley/springbootserver/Feedback.java
+++ src/main/java/tech/lashley/springbootserver/Feedback.java
@@ -3,24 +3,61 @@ package tech.lashley.springbootserver;
 import java.io.File;
 import java.util.Scanner;
 
+import org.json.simple.JSONObject;
+
 
 public class Feedback {
-    private String content;
-    public Feedback() {
-        //Modify to feed file into this constructor, data then passed to Spring Application
-        this.content = readText();
+
+	//Feed file then pass to Spring Application
+    public String getFeedback() {
+        return readFeedback();
     }
-    public String getContent() {
-        return content;
+    
+    //Method returns resume file as string
+    public String getResume() {
+    	return resumeFile();
+    }
+    
+    //Method to return resume in JSON format
+    public JSONObject getJSONResume() {
+    	JSONObject jsonObject = new JSONObject();
+    	//Convert JSON resume text into jsonObject
+    	
+    	
+    	return jsonObject;   	
+    }
+    
+    //sends the resume.txt file
+    private String resumeFile() {
+    	File file = null;
+    	Scanner scan = null;
+	    String resumeText = "";
+	    try {
+	        file = new File("/home/pi/Public/feedback/resume.txt");
+		    scan = new Scanner(file);
+	    } catch(Exception e){
+	        System.out.println(e);
+	    }
+
+	    //while there's a next line, add it to output
+	    try {
+	        while (scan.hasNextLine()) {
+	            resumeText += (scan.nextLine()+" ");
+	        }
+		    scan.close();
+	    } catch(Exception e){
+	        System.err.println(e);
+	    }
+    	return resumeText;
     }
 
     //reads the feedback.txt file
-    private String readText(){
+    private String readFeedback(){
     	File file = null;
     	Scanner scan = null;
 	    String feedbackText = "";
 	    try {
-	        file = new File("src/home/pi/Public/feedback/feedback.txt");
+	        file = new File("/home/pi/Public/feedback/feedback.txt");
 		    scan = new Scanner(file);
 	    } catch(Exception e){
 	        System.out.println(e);
--- src/main/java/tech/lashley/springbootserver/SenderController.java
+++ src/main/java/tech/lashley/springbootserver/SenderController.java
@@ -1,5 +1,6 @@
 package tech.lashley.springbootserver;
 
+import org.json.simple.JSONObject;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @RestController
 public class SenderController {
+    Feedback feedback = new Feedback();
 
     @RequestMapping("/feedbacksender")
-    public String sender() {
-        Feedback feedback = new Feedback();
-        return feedback.getContent();
+    public String feedbackSender() {
+        return feedback.getFeedback();
     }
     
+    @RequestMapping("/resume")
+    public String resumeSender() {
+        return feedback.getResume();
+    }
+    
+    @RequestMapping(value="/resumeJSON", produces="application/json")
+    public JSONObject jsonResume() {
+        return feedback.getJSONResume();
+    }
 }
